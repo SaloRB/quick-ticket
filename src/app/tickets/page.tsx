c7803d9b635dd229285473c09a6a00d73e1e03ca
Salomon Romano <salo_romano@yahoo.com>
2025-05-21 16:17:45 -0600

    feat: refactor ticket listing to use TicketItem component for improved readability and maintainability

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,9 +1,9 @@
-import Link from "next/link";
 import { redirect } from "next/navigation";
 
 import { getTickets } from "@/actions/ticket.actions";
 import { getCurrentUser } from "@/lib/current-user";
-import { getPriorityClass } from "@/utils/ui";
+
+import TicketItem from "../components/ticket-item";
 
 async function TicketsPage() {
   const user = await getCurrentUser();
@@ -22,29 +22,7 @@ async function TicketsPage() {
         : (
             <div className="space-y-4 max-w-3xl mx-auto">
               {tickets.map(ticket => (
-                <div className="flex justify-between items-center bg-white rounded-lg shadow border border-gray-200 p-6" key={ticket.id}>
-                  {/* Left Side */}
-                  <div>
-                    <h2 className="text-xl font-semibold text-blue-600">{ticket.subject}</h2>
-                  </div>
-
-                  {/* Right Side */}
-                  <div className="text-right space-y-2">
-                    <div className="text-sm text-gray-500">
-                      Priority:
-                      {" "}
-                      <span className={getPriorityClass(ticket.priority)}>
-                        {ticket.priority}
-                      </span>
-                    </div>
-                    <Link
-                      href={`/tickets/${ticket.id}`}
-                      className="inline-block mt-2 bg-blue-600 text-white text-sm px-3 py-1 rounded hover:bg-blue-700 transition text-center cursor-pointer"
-                    >
-                      View Ticket
-                    </Link>
-                  </div>
-                </div>
+                <TicketItem key={ticket.id} ticket={ticket} />
               ))}
             </div>
           )}
